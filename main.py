b3ec4c6b56a1a84e2fae94ba86be800a002befec
beelzebul-hub1 <stephendavis664+1@gmail.com>
2026-07-26 22:46:52 -0500

    Update Twitch miner credentials

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ threading.Thread(target=run_flask, daemon=True).start()
 
 # ---------------- MINER ----------------
 twitch_miner = TwitchChannelPointsMiner(
-    username="Cipher11483",
-    password="Qualify2-Shore6-Dismantle6-Fidgety7-Transfer4",
+    username="Haze_87697",
+    password="Rubber1-Relearn2-Catfight1-Gills7-Refold1",
     claim_drops_startup=False,
     priority=[
         Priority.STREAK,
